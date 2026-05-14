8ed77284cfebd7a7b5e835063edd78672b70028d
lukejbassett <bassettlukej@gmail.com>
2026-05-14 21:41:26 +0100

    + Page routing, nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <></>;
-}
+import { Route, Routes } from "react-router-dom";
+import { Homepage } from "./pages/Homepage";
+import { DriversPage } from "./pages/DriversPage";
+import { TeamsPage } from "./pages/TeamsPage";
+import { GamesPage } from "./pages/GamesPage";
+import { AboutPage } from "./pages/AboutPage";
+import { CircuitsPage } from "./pages/CircuitsPage";
+import { Layout } from "./components/Layout";
 
-export default App;
+export function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/drivers" element={<DriversPage />}></Route>
+          <Route path="/teams" element={<TeamsPage />}></Route>
+          <Route path="/races" element={<CircuitsPage />}></Route>
+          <Route path="/games" element={<GamesPage />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./index.css";
-import App from "./App.tsx";
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
